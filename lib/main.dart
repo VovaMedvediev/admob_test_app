dbb0a49b419ea482fc3dac2accd2690b7536e913
Volodymyr Medvediev <vovkamedvedev5@gmail.com>
2024-09-26 17:47:42 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: AdMobExample(),
+    );
+  }
+}
+
+class AdMobExample extends StatefulWidget {
+  const AdMobExample({super.key});
+
+  @override
+  _AdMobExampleState createState() => _AdMobExampleState();
+}
+
+class _AdMobExampleState extends State<AdMobExample> {
+  BannerAd? _bannerAd;
+  InterstitialAd? _interstitialAd;
+  bool _isInterstitialReady = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _bannerAd = BannerAd(
+      adUnitId: 'ca-app-pub-3940256099942544/6300978111',
+      size: AdSize.fullBanner,
+      request: const AdRequest(),
+      listener: const BannerAdListener(),
+    )..load();
+
+    _loadInterstitialAd();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    _interstitialAd?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AdMob Example'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (_bannerAd != null)
+            Container(
+              alignment: Alignment.center,
+              width: _bannerAd!.size.width.toDouble(),
+              height: _bannerAd!.size.height.toDouble(),
+              child: AdWidget(ad: _bannerAd!),
+            ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: _showInterstitialAd,
+            child: const Text('Show Interstitial Ad'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _loadInterstitialAd() {
+    InterstitialAd.load(
+      adUnitId: 'ca-app-pub-3940256099942544/1033173712',
+      request: const AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: (ad) {
+          _interstitialAd = ad;
+          _isInterstitialReady = true;
+        },
+        onAdFailedToLoad: (error) {
+          _isInterstitialReady = false;
+        },
+      ),
+    );
+  }
+
+  void _showInterstitialAd() {
+    if (_isInterstitialReady && _interstitialAd != null) {
+      _interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
+        onAdDismissedFullScreenContent: (InterstitialAd ad) {
+          ad.dispose();
+          _loadInterstitialAd(); // Load a new ad once the current one is shown
+        },
+        onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
+          ad.dispose();
+          _loadInterstitialAd(); // Load a new ad even if it fails
+        },
+      );
+
+      _interstitialAd!.show();
+      _isInterstitialReady = false;
+    }
+  }
+}
